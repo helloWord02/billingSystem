361d0788937546d7b46641b10ad797259a272cbf
Administrator <Administrator@192.168.2.106>
2017-11-28 13:40:43 +0800

    ServiceBean提供GET/Set方法

--- billingSystem/src/main/java/com/test_project/bean/ServiceBean.java
+++ billingSystem/src/main/java/com/test_project/bean/ServiceBean.java
@@ -7,6 +7,7 @@ import javax.persistence.Table;
 
 /**
  * 服务器信息实体Bean
+ * 
  * @author huangjun
  * @version 1.0
  * @created 25-11��-2017 15:09:23
@@ -21,17 +22,62 @@ public class ServiceBean {
 	private Date loginTime;
 	private long timeLong;
 
-	public ServiceBean(){
+	public ServiceBean() {
 
 	}
 
-	/**
-	 * 
-	 * @exception Throwable
-	 */
-	public void finalize()
-	  throws Throwable{
+	public String getBusniessAccount() {
+		return busniessAccount;
+	}
 
+	public void setBusniessAccount(String busniessAccount) {
+		this.busniessAccount = busniessAccount;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getIp() {
+		return ip;
+	}
+
+	public void setIp(int ip) {
+		this.ip = ip;
+	}
+
+	public Date getLoginoutTime() {
+		return loginoutTime;
+	}
+
+	public void setLoginoutTime(Date loginoutTime) {
+		this.loginoutTime = loginoutTime;
+	}
+
+	public Date getLoginTime() {
+		return loginTime;
+	}
+
+	public void setLoginTime(Date loginTime) {
+		this.loginTime = loginTime;
+	}
+
+	public long getTimeLong() {
+		return timeLong;
+	}
+
+	public void setTimeLong(long timeLong) {
+		this.timeLong = timeLong;
+	}
+
+	@Override
+	public String toString() {
+		return "ServiceBean [busniessAccount=" + busniessAccount + ", id=" + id + ", ip=" + ip + ", loginoutTime="
+				+ loginoutTime + ", loginTime=" + loginTime + ", timeLong=" + timeLong + "]";
 	}
 
 }
